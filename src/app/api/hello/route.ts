9782f5253347897209ba37cdf9698438ece163ae
Werapat <hellp002@gmail.com>
2024-05-26 14:09:49 +0700

    feat : test api

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,6 +2,5 @@ import db from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const res = await db.project.findMany();
   return Response.json({ message: "hello" });
 }

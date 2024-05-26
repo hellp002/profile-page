6fda7ac452072a8b42c6c7af6e285920dc1a3082
Werapat <hellp002@gmail.com>
2024-05-26 23:12:41 +0700

    change : favicon and fix navbar hover bug

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,12 +14,11 @@ export function Navbar() {
       <ul className="flex items-center w-3/5 md:w-2/5 lg:w-1/5 h-full">
         {linkObj.map((obj) => {
           return (
-            <li
-              key={obj.href}
-              className="hover:bg-red-400 py-6 px-2 md:p-6 h-full"
-            >
-              <Link href={obj.href}>{obj.text}</Link>
-            </li>
+            <Link href={obj.href} key={obj.href}>
+              <li className="hover:bg-red-400 py-6 px-2 md:p-6 h-full">
+                {obj.text}
+              </li>
+            </Link>
           );
         })}
       </ul>

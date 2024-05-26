75471ce83ff3be063bbeab74fd4251c347dc2962
Werapat <hellp002@gmail.com>
2024-05-26 12:14:25 +0700

    feat : edit yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
-  basePath: isProd ? "/profile-page" : "",
-  assetPrefix: isProd ? "/profile-page/" : "",
-  images: {
-    loader: "imgix",
-    path: isProd ? "/profile-page/" : "",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

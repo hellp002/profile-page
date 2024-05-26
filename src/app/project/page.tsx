e5347c2f386e9f3546acefd0f155b825d73ea49b
Werapat <hellp002@gmail.com>
2024-05-26 18:55:47 +0700

    fix : ci/cd

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -2,6 +2,10 @@ export default async function Project() {
   const data = await fetch(process.env.BACKEND_URL + "certificate", {
     method: "GET",
   });
-  const jsonData = await data.json();
-  return <div>{JSON.stringify(jsonData)}</div>;
+  if (data.status == 200) {
+    const jsonData = await data.json();
+    return <div>{JSON.stringify(jsonData)}</div>;
+  } else {
+    return <div>Failed</div>;
+  }
 }

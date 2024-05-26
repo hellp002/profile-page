8203ee1631f3c3c556ef2eaaa7e25feafa30d82f
Werapat <hellp002@gmail.com>
2024-05-26 15:00:44 +0700

    fix : api

--- src/app/api/helloworld/route.ts
+++ src/app/api/helloworld/route.ts
@@ -2,18 +2,21 @@ import db from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
-const CreateSchema = z.object({
+export const CreateSchema = z.object({
   from: z.string().min(1).email(),
   to: z.string().min(1).email(),
   subject: z.string().min(1),
 });
 
-type Creator = z.infer<typeof CreateSchema>;
+export type Creator = z.infer<typeof CreateSchema>;
+
+/*
 
 export async function GET(req: NextRequest) {
   const res = await db.project.findMany();
   return Response.json(res);
 }
+*/
 
 export async function POST(req: NextRequest) {
   const data = await req.json();

71a8f55be2dd4ca58eb388cfbf072af9203c347e
Werapat <hellp002@gmail.com>
2024-05-27 19:54:39 +0700

    feat : style navbar on path

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const linkObj = [
   { href: "./", text: "Home" },
@@ -9,13 +10,20 @@ const linkObj = [
 ];
 
 export function Navbar() {
+  const url = usePathname();
+  const compare = "." + url;
   return (
     <nav className="flex justify-between w-full bg-indigo-600 bg-opacity-70 sticky text-white">
       <ul className="flex items-center w-3/5 md:w-2/5 lg:w-1/5 h-full">
         {linkObj.map((obj) => {
           return (
             <Link href={obj.href} key={obj.href}>
-              <li className="hover:bg-red-400 py-6 px-2 md:p-6 h-full">
+              <li
+                className={
+                  "hover:bg-red-400 py-6 px-2 md:p-6 h-full " +
+                  (compare === obj.href ? "bg-slate-400" : "")
+                }
+              >
                 {obj.text}
               </li>
             </Link>
